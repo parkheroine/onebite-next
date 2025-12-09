5b76b3091e3d861abac1dbf5abe038f7e167dbd8
heroine <juyen3409@gmail.com>
2025-12-09 17:30:54 +0900

    feat: SEO 설정

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 import BookItem from '@/components/book-item'
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
 import fetchBooks from '@/lib/fetch-books'
+import Head from 'next/head'
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const q = context.query.q
@@ -16,6 +17,12 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 export default function Page({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입북스에 등록된 도서들을 만나보세요" />
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
