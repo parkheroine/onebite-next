c2123ee59441c216db89b7b029ec76d33eceff59
heroine <juyen3409@gmail.com>
2025-12-09 15:03:29 +0900

    feat: page routing, ssr 구현

---
+++ src/pages/book/[id].tsx
@@ -0,0 +1,33 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+import style from './[id].module.css'
+import fetchOneBook from '@/lib/fetch-one-book'
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const id = context.params!.id
+  const book = await fetchOneBook(Number(id))
+
+  return {
+    props: { book },
+  }
+}
+
+export default function Page({ book }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) {
+    return '해당 도서가 없습니다'
+  }
+  const { title, subTitle, description, author, publisher, coverImgUrl } = book
+  return (
+    <div className={style.container}>
+      <div className={style.cover_img_container} style={{ backgroundImage: `url(${coverImgUrl})` }}>
+        <img src={coverImgUrl} />
+      </div>
+
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </div>
+  )
+}
