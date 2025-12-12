a190759a97250876d504306fe255fa498c9abec9
heroine <juyen3409@gmail.com>
2025-12-12 16:58:16 +0900

    feat: server action, revalidatePath

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,14 +1,15 @@
-import { BookData } from '@/types'
+import { BookData, ReviewData } from '@/types'
 import style from './page.module.css'
 import { notFound } from 'next/navigation'
+import ReviewItem from '@/components/review-item'
+import ReviewEditor from '@/components/review-editor'
 
 export function generateStaticParams() {
   return [{ id: '1' }, { id: '2' }, { id: '3' }]
 }
 
-export default async function Page({ params }: { params: Promise<{ id: string | string[] }> }) {
-  const { id } = await params
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`)
+async function BookDetail({ bookId }: { bookId: string }) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`)
 
   if (!response.ok) {
     if (response.status === 404) {
@@ -21,7 +22,7 @@ export default async function Page({ params }: { params: Promise<{ id: string | 
   const { title, subTitle, description, author, publisher, coverImgUrl } = book
 
   return (
-    <div className={style.container}>
+    <section>
       <div className={style.cover_img_container} style={{ backgroundImage: `url('${coverImgUrl}')` }}>
         <img src={coverImgUrl} />
       </div>
@@ -31,6 +32,34 @@ export default async function Page({ params }: { params: Promise<{ id: string | 
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
+    </section>
+  )
+}
+
+async function ReviewList({ bookId }: { bookId: string }) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`)
+
+  if (!response.ok) {
+    throw new Error(`Review fetch failed : ${response.statusText}`)
+  }
+
+  const reviews: ReviewData[] = await response.json()
+  return (
+    <section>
+      {reviews.map((review) => (
+        <ReviewItem key={`review-item-${review.id}`} {...review} />
+      ))}
+    </section>
+  )
+}
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={id} />
+      <ReviewEditor bookId={id} />
+      <ReviewList bookId={id} />
     </div>
   )
 }
